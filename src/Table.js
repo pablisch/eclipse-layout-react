784aa7628582737d44dcebd6ac35d5c5c3a0ab8c
Pablo <pablito@me.com>
2023-11-15 20:37:54 +0000

    Updated player code in table to give the correct player labelling for 2, 3 and 6 players

--- src/Table.js
+++ src/Table.js
@@ -35,29 +35,6 @@ function Table({ generate, playerList, generatedCount }) {
   
   function fivePlayerRotation() {
     return (Math.floor(Math.random() * (5 - 1 + 1) + 1)) * 72 - 15
-    // if (playerList.length === 5) {
-    //   var randomNumber = Math.floor(Math.random() * (5 - 1 + 1) + 1);
-    //   switch (randomNumber) {
-    //     case 1:
-    //       setTableImage('images/playTableFiveV1b.png');
-    //       break;
-    //     case 2:
-    //       setTableImage('images/playTableFiveV2b.png');
-    //       break;
-    //     case 3:
-    //       setTableImage('images/playTableFiveV3b.png');
-    //       break;
-    //     case 4:
-    //       setTableImage('images/playTableFiveV4b.png');
-    //       break;
-    //     case 5:
-    //       setTableImage('images/playTableFiveV5b.png');
-    //       break;
-    //     default:
-    //       setTableImage('images/playTable.png');
-    //       break;
-    //   }
-    // }
   }
 
   function shuffleArray(array) {
@@ -86,10 +63,13 @@ function Table({ generate, playerList, generatedCount }) {
         
         <section className="left-side">
           <section className="boxes">
-            <div className="container">
-              <div className="box three">
+              <div className="container">
+              {(generatedCount > 0 && playerList.length < 3) ? null : (
+                <div className="box three">
                 <h2 id = "three"><i className="fa-solid fa-ghost"></i>{playerList[2] ? playerList[2] : 'Player 3'}</h2>
               </div>
+                )}
+              
             </div>
           </section>
         </section>
@@ -108,7 +88,6 @@ function Table({ generate, playerList, generatedCount }) {
             </div>
           </section>
             <section className="table">
-              {/* {tableImage && <img id='old-table' src={tableImage} alt='table' />} */}
               <img id='table-base' src='images/playTable.png' alt="table-base" />
               <img src={`images/eclipse-layout-${numOfPlayers}.png`} alt="Eclipse layout for 6 players" id='layout' style={rotation} />
           </section>
@@ -117,10 +96,14 @@ function Table({ generate, playerList, generatedCount }) {
                 <div className="box two">
                   <h2 id ="two"><i className="fa-solid fa-ghost"></i>{playerList[1] ? playerList[1] : 'Player 2'}</h2>
                 </div>
-                <div className="box four">
+                {(generatedCount > 0 && playerList.length < 4) ? null : (
+                  <div className="box four">
                   <h2 id = "four"><i className="fa-solid fa-ghost"></i>{playerList[3] ? playerList[3] : 'Player 4'}</h2>
                 </div>
-                {(generatedCount > 0 && playerList.length < 6) ? null : (<div className="box six">
+                )}
+                
+                {(generatedCount > 0 && playerList.length < 6) ? null : (
+                  <div className="box six">
                   <h2 id = "six"><i className="fa-solid fa-ghost"></i>{playerList[5] ? playerList[5] : 'Player 6'}</h2>
                 </div>)}
                 
